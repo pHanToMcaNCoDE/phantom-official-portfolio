b8cb08e22889c9d741b62d2b465b9d4d75ecfcdd
Victory Odumeh <phantom@Victorys-MacBook-Air.local>
2022-11-12 20:11:57 +0100

    saved

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 let menu = document.querySelector('.menu');
 let links = document.querySelector('.links');
+let x = document.querySelector('.x');
 
 menu.addEventListener('click', () => {
     links.classList.add('active');
     links.style.transition = ".5s";
+});
+
+x.addEventListener('click', () =>{
+    links.classList.remove('active');
+    links.style.transition = '.5s';
 });
